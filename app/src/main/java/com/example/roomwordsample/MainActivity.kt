1591ace3784c90659c6f3b7472375e348d2f1663
Anh Vu Pham <a_pham@utb.cz>
2020-12-09 17:55:09 +0100

    implementation of NewWordActivity.kt, WordsApplication.kt. Update activity_new_word.xml

--- app/src/main/java/com/example/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -1,27 +1,70 @@
 package com.example.roomwordsample
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private val newWordActivityRequestCode = 1
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        wordViewModel.allWords.observe(owner = this) { words ->
+            // Update the cached copy of the words in the adapter.
+            words.let { adapter.submitList(it) }
+        }
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            intentData?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let { reply ->
+                val word = Word(reply)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+}
+
     class WordListAdapter : ListAdapter<Word, WordListAdapter.WordViewHolder>(WordsComparator()) {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
@@ -32,6 +75,7 @@ class MainActivity : AppCompatActivity() {
             val current = getItem(position)
             holder.bind(current.word)
         }
+
 
         class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             private val wordItemView: TextView = itemView.findViewById(R.id.textView)
@@ -58,5 +102,5 @@ class MainActivity : AppCompatActivity() {
                 return oldItem.word == newItem.word
             }
         }
+
     }
-}
--- app/src/main/java/com/example/roomwordsample/Word.kt
+++ app/src/main/java/com/example/roomwordsample/Word.kt
@@ -5,10 +5,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "word_table")
-class Word(
-
-    @PrimaryKey(autoGenerate = true) val id: Int,
-
-    @ColumnInfo(name = "word") val word: String
-
-)
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
