47c67907a54d667c285aecbadc7479387bb01234
Anh Vu Pham <a_pham@utb.cz>
2020-12-09 17:04:07 +0100

    implementation of DAO and entities

---
+++ app/src/main/java/com/example/roomwordsample/Word.kt
@@ -0,0 +1,14 @@
+package com.example.roomwordsample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(
+
+    @PrimaryKey(autoGenerate = true) val id: Int,
+
+    @ColumnInfo(name = "word") val word: String
+
+)
